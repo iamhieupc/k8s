d329a873c4abeaf9ded19cef262696917844f805
hustchihieu143 <chihieusky@gmail.com>
2022-07-15 11:39:39 +0700

    add docker

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Up and Running on port ${port}- This is Customer service`);
+})
